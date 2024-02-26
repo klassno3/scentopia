987ec6490a4e6c4ea88b7846b69f36003773c0d2
betelhem <betelhemkirub@gmail.com>
2024-02-26 15:53:39 +0300

    Finished navigation added search component and product list

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ProductList() {
+  return (
+    <div>
+      <div id="womenPerfume"></div>
+      <div id="menPerfume"></div>
+      <div id="bestSeller"></div>
+      <div id="brands"></div>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Discount from '../components/Discount'
 import Brands from '../components/Brands'
+import ProductList from '../components/ProductList'
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Discount />
+      <ProductList/>
       <Brands/>
       
     </div>
