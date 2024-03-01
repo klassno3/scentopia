e7c836192a03cc64b21ba0d581e1d5790cfbf298
zekud <zekudk@gmail.com>
2024-03-01 16:59:15 +0300

    editing style

--- src/components/ProductList/WomenPerfumeList.jsx
+++ src/components/ProductList/WomenPerfumeList.jsx
@@ -17,8 +17,30 @@ export default function WomenPerfumeList({ data }) {
   return (
     <div className="flex flex-col h-auto w-full">
       <div className="flex flex-col space-y-4 p-5">
-        <div className="flex flex-col gap-4 justify-center items-center text-center md:flex-row">
+        {/* <div className="flex flex-col gap-4 justify-center items-center text-center md:flex-row">
           <h1 className="text-3xl font-lobsterTwo font-bold text-black w-full  md:text-center mr-2">
+            Women's{" "}
+            <span className="border-b-2 border-accentPink-dark">Perfume</span>
+          </h1>
+          <div>
+            <select
+              name="filter"
+              id="filter"
+              className="text-black p-2 bg-white rounded  focus:outline-none border-2 border-accentPink-dark md:ml-auto"
+            >
+              <option value="" selected disabled>
+                Filter
+              </option>
+              <option ption value="brand">
+                Brand
+              </option>
+              <option value="price">Price</option>
+              <option value="arriveddate">Arrived Date</option>
+            </select>
+          </div>
+        </div> */}
+        <div className="flex justify-between items-center md:justify-center ">
+          <h1 className="text-3xl font-lobsterTwo font-bold text-black w-full  md:text-center mr-2  ">
             Women's{" "}
             <span className="border-b-2 border-accentPink-dark">Perfume</span>
           </h1>
